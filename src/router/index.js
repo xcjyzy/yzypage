a1681382b65ee86e850af21321335054527d4b33
yzy <3308610653@qq.com>
2022-08-07 14:19:16 +0800

    [main]delete vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import index from '../views/index.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: index
   },
   {
     path: '/about',
